f07bc11d6b53a829b6c1f749c9e300fda32e6a16
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2023-02-22 21:15:14 -0600

    Genotype Matrix: Add basic test and test READMEs.

---
+++ trpgeno_genomatrix/tests/src/Functional/InstallTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\Tests\trpgeno_genomatrix\Functional;
+
+use Drupal\Core\Url;
+use Drupal\Tests\tripal_chado\Functional\ChadoTestBrowserBase;
+
+/**
+ * Simple test to ensure that main page loads with module enabled.
+ *
+ * @group Tripal Genotype Matrix
+ * @group Installation
+ */
+class InstallTest extends ChadoTestBrowserBase {
+
+  protected $defaultTheme = 'stable';
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = ['trpgeno_genetics', 'trpgeno_genotypes', 'trpgeno_genomatrix'];
+
+  /**
+   * A user with permission to administer site configuration.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $user;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->user = $this->drupalCreateUser(['administer site configuration']);
+    $this->drupalLogin($this->user);
+  }
+
+  /**
+   * Tests that the home page loads with a 200 response.
+   */
+  public function testLoad() {
+    $this->drupalGet(Url::fromRoute('<front>'));
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}
